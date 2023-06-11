f9210d789999bad31ed9599082307e9bd2270f0d
kurotakazuki <kabisakanakabi@gmail.com>
2023-06-11 19:24:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# voicevox_with_flutter
+
+Voicevox をFlutter（android emulator）で動かす例
+
+voicevox のサーバを別途立ち上げる必要がある
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:voicevox_with_flutter/voicevox.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter VOICEVOX Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'VOICEVOX with Flutter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  VoiceVoxConnection vv = VoiceVoxConnection(1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var audio = await vv.textToAudioClip("こんにちずんずん ずんだめんなのだ");
+          print(audio.lengthInBytes);
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
